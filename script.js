17b6f1092796fae98f7c154f67955486207e1ce8
Sujit Kushwaha <120260015+tm-empire@users.noreply.github.com>
2026-04-10 18:10:08 -0700

    Refactor task management and add local storage support
    Refactor task management logic and integrate local storage for input persistence. Added functionality to track task completion and update progress dynamically.

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const inputsTag = document.querySelectorAll('.input');
 const taskContainer = document.querySelector('.task-container');
-
-
-inputsTag.forEach((eachElement) => {
-    eachElement.addEventListener('blur', (e) => {
-        console.log(eachElement.value);
-        eachElement.innerText = eachElement.value;
-    });
-});
-
 const setGoalText = document.querySelector('.about-set-goal p');
 
 const checkButton = document.querySelectorAll('.circle');
@@ -22,44 +13,111 @@ const progressValue = document.querySelector('.progress-value');
 const progressParagraph = document.querySelector('.progress-value p')
 let countTaskDone = 0;
 
+const enteredData = JSON.parse(localStorage.getItem('enteredData')) || {};
+
+const styleDataStatus = JSON.parse(localStorage.getItem('styleDataStatus')) || {};
+
+for (let i = 0; i < inputsTag.length; i++) {
+    inputsTag[i].value = enteredData[i];
+    if (styleDataStatus[i]) {
+        // console.log("love you sujit")
+        inputsTag[i].classList.toggle('clickedEffect');
+        checkButton[i].classList.toggle('checkIndication');
+        checkButton[i].classList.add('done');
+        countTaskDone = countTaskDone + 1;
+        progressValue.style.maxWidth = `${1 / checkButton.length * 100 * countTaskDone}%`;
+        progressParagraph.innerText = `${countTaskDone}/${checkButton.length} completed`
+
+    }
+}
+
+
+inputsTag.forEach((eachElement, index) => {
+    eachElement.addEventListener('blur', (e) => {
+        // console.log(eachElement.value);
+        enteredData[index] = eachElement.value;
+        localStorage.setItem('enteredData', JSON.stringify(enteredData));
+        eachElement.innerText = eachElement.value;
+    });
+});
+
+
+
 
 
 
 taskContainer.addEventListener('click', (e) => {
-    if ((inputsTag[0].value !== '') && (inputsTag[1].value !== '') && (inputsTag[2].value !== '')) {
+    const TruthyFalsyValue = [...inputsTag].every((eachElement) => {
+        return eachElement.value.trim() !== '';
+    });
+    if (TruthyFalsyValue) {
 
         for (let i = 0; i < checkButton.length; i++) {
             if (e.target === checkButton[i] || e.target === svgList[i]) {
-                if (countTaskDone < checkButton.length) {
+                // checking the checkbutton toggle or not according to this it will be change
+                const isCompleted = checkButton[i].classList.contains('done');
+                if (!isCompleted) {
                     countTaskDone = countTaskDone + 1;
-                    progressValue.style.maxWidth = `${33.33*countTaskDone}%`;
+                    progressValue.style.maxWidth = `${1 / checkButton.length * 100 * countTaskDone}%`;
                     progressParagraph.innerText = `${countTaskDone}/${checkButton.length} completed`
-                }
-                if (e.target === svgList[i]) {
-                    console.log(e.target.parentElement.nextElementSibling.value);
 
-                    if (e.target.parentElement.nextElementSibling.value !== '') {
-                        console.log(e.target);
-                        e.target.parentElement.nextElementSibling.style.color = "red";
-                        e.target.parentElement.nextElementSibling.style.textDecoration = "line-through";
-                        e.target.parentElement.style.backgroundColor = "#48A300";
-                        goalText.innerText = "Just a step away, keep going!";
-                        bottomText.innerText = "“Keep Going, You’re making great progress!”";
+                    if (e.target === svgList[i]) {
+                        console.log(e.target.parentElement.nextElementSibling.value);
+
+                        if (e.target.parentElement.nextElementSibling.value !== '') {
+                            e.target.parentElement.nextElementSibling.classList.toggle('clickedEffect');
+                            e.target.parentElement.classList.toggle('checkIndication');
+                            e.target.parentElement.classList.add('done');
+                            goalText.innerText = "Just a step away, keep going!";
+                            bottomText.innerText = "“Keep Going, You’re making great progress!”";
+                        }
                     }
+                    else {
+                        console.log(e.target.nextElementSibling.value);
+
+                        if (e.target.nextElementSibling.value !== '') {
+                            e.target.nextElementSibling.classList.toggle('clickedEffect');
+                            e.target.classList.toggle('checkIndication');
+                            e.target.classList.add('done');
+                            goalText.innerText = "Just a step away, keep going!";
+                            bottomText.innerText = "“Keep Going, You’re making great progress!”";
+
+                        }
+                    }
+
                 }
+
                 else {
-                    console.log(e.target.nextElementSibling.value);
+                    countTaskDone = countTaskDone - 1;
+                    progressValue.style.maxWidth = `${1 / checkButton.length * 100 * countTaskDone}%`;
+                    progressParagraph.innerText = `${countTaskDone}/${checkButton.length} completed`
 
-                    if (e.target.nextElementSibling.value !== '') {
-                        console.log(e.target);
-                        e.target.nextElementSibling.style.color = "red";
-                        e.target.nextElementSibling.style.textDecoration = "line-through";
-                        e.target.style.backgroundColor = "#48A300";
-                        goalText.innerText = "Just a step away, keep going!";
-                        bottomText.innerText = "“Keep Going, You’re making great progress!”";
+                    if (e.target === svgList[i]) {
+                        console.log(e.target.parentElement.nextElementSibling.value);
 
+                        if (e.target.parentElement.nextElementSibling.value !== '') {
+                            e.target.parentElement.nextElementSibling.classList.toggle('clickedEffect');
+                            e.target.parentElement.classList.toggle('checkIndication');
+                            e.target.parentElement.classList.remove('done');
+                            goalText.innerText = "Just a step away, keep going!";
+                            bottomText.innerText = "“Keep Going, You’re making great progress!”";
+                        }
                     }
+                    else {
+                        console.log(e.target.nextElementSibling.value);
+
+                        if (e.target.nextElementSibling.value !== '') {
+                            e.target.nextElementSibling.classList.toggle('clickedEffect');
+                            e.target.classList.toggle('checkIndication');
+                            e.target.classList.remove('done');
+                            goalText.innerText = "Just a step away, keep going!";
+                            bottomText.innerText = "“Keep Going, You’re making great progress!”";
+
+                        }
+                    }
+
                 }
+
             }
         }
 
@@ -71,6 +129,22 @@ taskContainer.addEventListener('click', (e) => {
             }
         }
     }
+
+
+    for (let s = 0; s < checkButton.length; s++) {
+        const ifCompleted = checkButton[s].classList.contains('done');
+        if (ifCompleted) {
+            styleDataStatus[s] = true;
+            localStorage.setItem('styleDataStatus', JSON.stringify(styleDataStatus));
+        }
+        else {
+            styleDataStatus[s] = false;
+            localStorage.setItem('styleDataStatus', JSON.stringify(styleDataStatus));
+        }
+    }
+
+    congractulation();
+
 });
 
 
@@ -79,19 +153,33 @@ inputsTag.forEach((eachElement) => {
         setGoalText.style.display = "none";
         for (let i = 0; i < inputsTag.length; i++) {
             if (e.target === inputsTag[i]) {
-                inputsTag[i].style.color = "black";
-                inputsTag[i].style.textDecoration = "none";
-                checkButton[i].style.backgroundColor = "#FBFBFB"
+                inputsTag[i].classList.remove('clickedEffect');
+                checkButton[i].classList.remove('checkIndication');
+            }
+            if (inputsTag[i] === e.target) {
+                const isCompleted = checkButton[i].classList.contains('done');
+                if (isCompleted) {
+                    countTaskDone = countTaskDone - 1;
+                    progressValue.style.maxWidth = `${1 / checkButton.length * 100 * countTaskDone}%`;
+                    progressParagraph.innerText = `${countTaskDone}/${checkButton.length} completed`
+                    checkButton[i].classList.remove('done')
+                }
             }
         }
+
     });
 });
 
-
-
-
-
-
+function congractulation() {
+    const allTrueNot = [...checkButton].every((eachElement) => {
+        return eachElement.classList.contains('done');
+    })
+    if(allTrueNot){
+         bottomText.innerText = "Congractulation for completion of targeted task";
+         alert('congractulation')
+    }
+}
+congractulation();
 
 
 
